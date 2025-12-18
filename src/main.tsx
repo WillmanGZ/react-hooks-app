37060e77242ba22f593c1a6b816d5a1c2f14f8b7
WillmanGZ <willmang@uninorte.edu.co>
2025-12-18 16:21:58 -0500

    feat: add PokemonPage component and custom hooks for counter and Pokemon data

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 /* import { HooksApp } from "./HooksApp"; */
 /* import { TrafficLight } from "./useState/TrafficLight"; */
 /* import { TrafficLightWithEffect } from "./useEffect/TrafficLightWithEffect"; */
-import { TrafficLightWithHook } from "./useEffect/TrafficLightWithHook";
+/* import { TrafficLightWithHook } from "./useEffect/TrafficLightWithHook"; */
+/* import { PokemonPage } from "./examples/PokemonPage";*/
+/* import { FocusScreen } from "./useRef/FocusScreen"; */
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
@@ -11,6 +13,8 @@ createRoot(document.getElementById("root")!).render(
     {/* <HooksApp /> */}
     {/*     <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
-    <TrafficLightWithHook />
+    {/* <TrafficLightWithHook /> */}
+    {/* <PokemonPage /> */}
+    {/* <FocusScreen /> */}
   </StrictMode>
 );
